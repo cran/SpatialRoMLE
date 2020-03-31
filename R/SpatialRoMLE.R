13a0ed70762d8de889643ecf3436af2d9d78b315
Vural Yildirim <vurall_yildirim@hotmail.com>
2020-03-31 11:50:05 +0000

    version 0.1.0

---
+++ R/SpatialRoMLE.R
@@ -0,0 +1,13 @@
+#' Spatial Robust MLE Package
+#'
+#' @description Robust Maximum Likelihood Estimation for Spatial Error Model.
+#'
+#' @docType package
+#'
+#' @author Vural Yildirim \email{vurall_yildirim@hotmail.com}
+#' @author Yeliz Mert Kantar
+#'
+#' @references Yildirim, V. and Kantar, Y.M. (2020). Robust estimation of spatial error model. in Journal of Statistical Computation and Simulation. \url{https://doi.org/10.1080/00949655.2020.1740223}
+#'
+#' @name SpatialRoMLE
+NULL
